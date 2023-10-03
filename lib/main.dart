2a510bba7c3e85acd6af8584f4203d196ff0135e
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2023-10-02 22:28:44 -0300

    update user interface home

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'home.dart';
 void main() async {
   runApp(MaterialApp(
     home: Home(),
-    theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.amber),
+    theme: ThemeData(hintColor: Colors.grey, primaryColor: Colors.blue),
+    debugShowCheckedModeBanner: false,
   ));
 }
